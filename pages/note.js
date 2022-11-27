c21a2ce07b412b567dd938b4f21b3ce61d88881e
pedrodantas0 <pedrodantas2943@gmail.com>
2022-11-26 21:42:29 -0300

    '12'

--- pages/note.js
+++ pages/note.js
@@ -2,8 +2,7 @@ import firebase from '../lib/firebase'
 import useAuth from '../hooks/useAuth'
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Note.module.css'
 
 export default function Note() {
   const { user, signout } = useAuth()
@@ -12,7 +11,11 @@ export default function Note() {
   const [ note, setNote ] = useState('')
   const [ notes, setNotes ] = useState()
   const [ key, setkey] = useState('')
+  const [ selected, setSelected ] = useState('')
   const [ updating, setUpdating ] = useState(false)
+  const testid = user?.email.slice(0, -10)
+  const [busca, setBusca] = useState()
+  const [estaBuscando, setEstaBuscando] = useState(false)
 
   function save(){
     const pasteTitleNote = {
@@ -20,7 +23,7 @@ export default function Note() {
       title,
       note
     }
-    firebase.database().ref(user?.email).push(pasteTitleNote)
+    firebase.database().ref(testid).push(pasteTitleNote)
     setPaste('')
     setTitle('')
     setNote('')
@@ -38,17 +41,39 @@ export default function Note() {
       'title': title,
       'note': note
     }
-    firebase.database().ref('notes').child(key).update(pasteTitleNote)
+    firebase.database().ref(testid).child(key).update(pasteTitleNote)
     setPaste('')
     setTitle('')
     setNote('')
     setUpdating(false)
   }
   function deleteNote(ref){
-    firebase.database().ref('notes/'+ref).remove()
+    firebase.database().ref(testid+'/'+ref).remove()
+  }
+  function buscar(event){
+    const palavra = event.target.value
+    if(palavra.length > 0) {
+      setEstaBuscando(true)
+    const dados = new Array
+
+    notes?.map(note => {
+      const regra = new RegExp(event.target.value, "gi")
+      if(regra.test(note.title)){
+        dados.push(note)
+      }
+    })
+
+    setBusca(dados)
+    } else{
+      setEstaBuscando(false)
+    }
+    
+  }
+  function selectNote(x){
+    setSelected(x)
   }
   useEffect(() => {
-    firebase.database().ref('notes').on('value', result => {
+    firebase.database().ref(testid).on('value', result => {
       const resultpasteTitleNote = Object.entries(result.val() ?? {}).map(([key, value]) => {
         return {
           'key': key,
@@ -72,40 +97,50 @@ export default function Note() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.ribbon}>
-          Economize na compra do Macbook Air Pro de 13 polegadas na Apple Store Educational*.
-          <a> Comprar &gt;</a>
+      <div>
+
+      <input type="text" placeholder="Search" onChange={buscar}></input>
+            {estaBuscando ? 
+             busca?.map(note => {
+              return (
+                <div key={note.key} onClick={() => selectNote(note.note)}>
+                  <button onClick={() => edit(note)}>Edit</button>
+                  <button onClick={() => deleteNote(note.key)}>Delete</button>
+                  <h2>{note.paste}</h2>
+                  <h1>{note.title}</h1>
+                </div>
+              )
+            }) : notes?.map(note => {
+              return (
+                <div key={note.key} onClick={() => selectNote(note.note)}>
+                  <button onClick={() => edit(note)}>Edit</button>
+                  <button onClick={() => deleteNote(note.key)}>Delete</button>
+                  <h2>{note.paste}</h2>
+                  <h1>{note.title}</h1>
+                </div>
+              )
+            })
+            
+            }
         </div>
-        <p className={styles.free}>Free</p>
-        <h1 className={styles.title}>{user?.email}</h1>
-        <h2 className={styles.subtitle}>Seu bloco de anotacoes digital</h2>
-        <p className={styles.description}>Um bloco de anotacoes multifucional para todas as suas necessidades.</p>
-        <div className={styles.buttons}>
-          <button onClick={() => signout()}>Comprar</button>
-          <a>Saiba mais &gt;</a>
+        <div className={styles.right}>
+          <div className={styles.buttons}>
+            <button onClick={() => signout()}>signout</button>
+          </div>
+          <div>
+            <form>
+              <input type="text" placeholder="Paste" value={paste} onChange={event => setPaste(event.target.value)}></input>
+              <input type="text" placeholder="Title" value={title} onChange={event => setTitle(event.target.value)}></input>
+              <input type="text" placeholder="Note" value={note} onChange={event => setNote(event.target.value)}></input>
+              {updating ? <button type="button" onClick={update}>Update</button> : <button type="button" onClick={save}>Save</button>}
+              
+            </form>
+            <div>
+              <p>{selected}</p>
+            </div>
+          </div>
         </div>
-        <Image src="/RE2lwga.png" alt="Vercel Logo" width={630} height={380} />
-        <form>
-          <input type="text" placeholder="Paste" value={paste} onChange={event => setPaste(event.target.value)}></input>
-          <input type="text" placeholder="Title" value={title} onChange={event => setTitle(event.target.value)}></input>
-          <input type="text" placeholder="Note" value={note} onChange={event => setNote(event.target.value)}></input>
-          {updating ? <button type="button" onClick={update}>Update</button> : <button type="button" onClick={save}>Save</button>}
-          
-        </form>
-        <div>
-          <input type="text" placeholder="Search"></input>
-          {notes?.map(note => {
-            return (
-              <div key={note.key}>
-                <button onClick={() => edit(note)}>Edit</button>
-                <button onClick={() => deleteNote(note.key)}>Delete</button>
-                <h2>{note.paste}</h2>
-                <h1>{note.title}</h1>
-                <p>{note.note}</p>
-              </div>
-            )
-          })}
-        </div>
+        
       </main>
 
       <footer className={styles.footer}>
